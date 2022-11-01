a1a65523185e7293939e60f4c3ab8194597918e3
muguilin <muguilin@foxmail.com>
2022-11-01 11:18:16 +0800

    update

---
+++ Ajax数据请求/Fetch API/fetch.js
@@ -0,0 +1,23 @@
+
+// fetch Api封装
+async function ajax(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST', // GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // 将JSON响应解析为JavaScript对象
+};
+
+// 调用
+ajax('https://xxx.com/api', { age: 18 }).then(data => {
+    console.log(data); // JSON data parsed by `data.json()` call
+});
