207794c99ffb0828a9336a25930a5585c6fcd544
muguilin <muguilin@foxmail.com>
2023-11-03 17:48:58 +0800

    update

--- ESM模块化/ESM模块的导入导出/module-1.js
+++ ESM模块化/ESM模块的导入导出/module-1.js
@@ -1,3 +1,18 @@
-import all from "./module-2";
+// import def from "./module-3.js";
+// console.log(def);
 
+import * as all from "./module-2.js";
 console.log(all);
+
+// import { PI, isOdd, Person } from "./module-3.js";
+// console.log(PI, isOdd(666), new Person('mupiao'));
+
+// import def, { PI as pi, isOdd, Person } from "./module-3.js";
+// console.log(def);
+// console.log(pi, isOdd(666), new Person('mupiao'));
+
+
+export { default as def, PI, isOdd, Person } from "./module-3.js";
+// console.log(def );
+
+
--- ESM模块化/ESM模块的导入导出/module-2.js
+++ ESM模块化/ESM模块的导入导出/module-2.js
@@ -3,11 +3,11 @@
  * 简单来讲：就是将 module-3.js模块文件中 所有被声明向外导出的 变量、常量、函数、对象属性等全部都原封不动的向外导出。
  * 大白话：就是在外部使用时，只要引用了当前 module-2.js 文件 同时也引用 module-3.js 
  */
-export * from "./module-3.js";
+// export * from "./module-3.js";
 
 
-import * as all from "./module-3.js";
-console.dir(all)
+// import * as all from "./module-3.js";
+// console.dir(all)
 
 // export { AudioTrackList, AudioTrackRequest } from './';
 
@@ -17,7 +17,12 @@ console.dir(all)
  */
 // export { default } from './module-3.js';
 
+
 /**
  * 导入导出默认模块 并更名为myDef
  */
 // export { default as myDef } from './module-3.js';
+
+export { default as def, PI, isOdd, Person } from './module-3.js';
+// console.log(MyDemo);
+
