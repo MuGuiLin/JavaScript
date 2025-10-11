294cce4b809dd3676fcf982df6d8c91d4fdd5a44
muguilin <muguilin@foxmail.com>
2025-10-11 09:42:11 +0800

    update

--- NET网络请求/Fetch API/fetch.js
+++ NET网络请求/Fetch API/fetch.js
@@ -2,19 +2,20 @@
 // fetch Api封装
 async function ajax(url = '', data = {}) {
     const response = await fetch(url, {
-        method: 'POST', // GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: 'same-origin', // include, *same-origin, omit
+        method: 'POST', // 请求方法： GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // 请求模式： no-cors, *cors, same-origin
+        cache: 'no-cache', // 缓存模式： *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // 请求凭证： include, *same-origin, omit
         headers: {
             'Content-Type': 'application/json'
             // 'Content-Type': 'application/x-www-form-urlencoded',
+            // 'Content-Type': 'multipart/form-data',
         },
-        redirect: 'follow', // manual, *follow, error
-        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
+        redirect: 'follow', // 重定向： manual, *follow, error
+        referrerPolicy: 'no-referrer', // 引用策略： no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) //  请求体： body data type must match "Content-Type" header
     });
-    return response.json(); // 将JSON响应解析为JavaScript对象
+    return response.json(); //  返回：将JSON响应解析为JavaScript对象
 };
 
 // 调用
